88c394f160c6323990c649a367ca44f87b268ddb
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-01 14:19:22 -0700

    Add mocking and stubing for museum test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require 'mocha/minitest'
 
 class MuseumTest <Minitest::Test
 
@@ -17,10 +18,12 @@ class MuseumTest <Minitest::Test
   end
 
   def test_it_can_add_exhibits
-    skip
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
-    imax = Exhibit.new("IMAX", 15)
+    # gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    # dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    # imax = Exhibit.new("IMAX", 15)
+    imax = mock("IMAX")
+    gems_and_minerals = mock("Gems and Minerals")
+    dead_sea_scrolls = mock("Dead Sea Scrolls")
     dmns = Museum.new("Denver Museum of Nature and Science")
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
@@ -31,18 +34,15 @@ class MuseumTest <Minitest::Test
 
 
   def test_patrons_starts_empty
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
-
     assert_equal [], dmns.patrons
   end
 
   def test_it_can_admit_patrons
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    bob = Patron.new("Bob", 20)
-    sally = Patron.new("Sally", 20)
+    bob = mock("Bob")
+    sally = mock("Sally")
 
     dmns.admit(bob)
     dmns.admit(sally)
@@ -51,46 +51,55 @@ class MuseumTest <Minitest::Test
   end
 
   def test_it_can_list_admitted_patrons_by_name
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    bob = Patron.new("Bob", 20)
-    sally = Patron.new("Sally", 20)
+    bob = mock("Bob")
+    sally = mock("Sally")
 
     dmns.admit(bob)
     dmns.admit(sally)
-
+    bob.stubs(:name).returns("Bob")
+    sally.stubs(:name).returns("Sally")
     assert_equal ["Bob","Sally"], dmns.patrons_by_name
   end
 
   def test_it_can_tell_you_the_average_exhibit_cost
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 4)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 11)
-    imax = Exhibit.new("IMAX", 15)
+    imax = mock("IMAX")
+    gems_and_minerals = mock("Gems and Minerals")
+    dead_sea_scrolls = mock("Dead Sea Scrolls")
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
+    
+    imax.stubs(:cost).returns(15)
+    gems_and_minerals.stubs(:cost).returns(5)
+    dead_sea_scrolls.stubs(:cost).returns(10)
 
     assert_equal 10, dmns.average_exhibit_cost
   end
 
   def test_there_are_recommended_exhibits_for_a_given_patron
-    ###CHALLENGE
-    skip
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
-    imax = Exhibit.new("IMAX", 15)
+    imax = mock("IMAX")
+    gems_and_minerals = mock("Gems and Minerals")
+    dead_sea_scrolls = mock("Dead Sea Scrolls")
     dmns = Museum.new("Denver Museum of Nature and Science")
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
-    bob = Patron.new("Bob", 20)
-    bob.add_interest("Dead Sea Scrolls")
-    bob.add_interest("Gems and Minerals")
-    sally = Patron.new("Sally", 20)
-    sally.add_interest("IMAX")
+
+    imax.stubs(:name).returns("IMAX")
+    gems_and_minerals.stubs(:name).returns("Gems and Minerals")
+    dead_sea_scrolls.stubs(:name).returns("Dead Sea Scrolls")
+    
+    bob = mock("Bob")
+    sally = mock("Sally")
+    # bob.add_interest("Dead Sea Scrolls")
+    # bob.add_interest("Gems and Minerals")
+    # sally.add_interest("IMAX")
+    bob.stubs(:interests).returns(["Dead Sea Scrolls", "Gems and Minerals"])
+    sally.stubs(:interests).returns(["IMAX"])
+    
     #hint: look at the recommend_exhibits() method and figure out which method you need to be called on which object, and then stub from there.
     assert_equal [gems_and_minerals,dead_sea_scrolls], dmns.recommend_exhibits(bob)
     assert_equal [imax], dmns.recommend_exhibits(sally)
